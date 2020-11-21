239b4fb8d94fba8a2c25cf506821a1eb66cd27b9
Johan Jansson <33960195+johan-jansson@users.noreply.github.com>
2020-11-21 16:49:54 +0100

    Update gentoo.sh

--- gentoo.sh
+++ gentoo.sh
@@ -39,5 +39,6 @@ mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-cp /root/gentoo-main/chroot.sh /mnt/gentoo/
-chroot /mnt/gentoo /bin/bash
+cp /root/gentoo-main/chroot.sh /mnt/gentoo/root/
+cp /root/gentoo.main/config /mnt/gentoo/root/
+chroot /mnt/gentoo /bin/bash /mnt/gentoo/root/./chroot.sh
