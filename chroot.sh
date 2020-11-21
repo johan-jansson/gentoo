01e98acb708a9b5c963a13361d5069b9a62a3275
Johan Jansson <33960195+johan-jansson@users.noreply.github.com>
2020-11-21 15:27:39 +0100

    Update and rename 2-chroot.sh to chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Gentoo installation script, optimized for AMD Ryzen 9 3900X with Zen 2 architecture (24 threads) 
+# Based on https://wiki.gentoo.org/wiki/Handbook:AMD64
+# Boot: install-amd64-minimal-*.iso
+#
+# wget https://github.com/johan-jansson/gentoo/archive/main.zip
+#
+# chroot.sh       # this script
+# gentoo.sh       # main script
+# wget https://github.com/johan-jansson/gentoo/make.conf        # portage config: /etc/portage/make.conf
+# wget https://github.com/johan-jansson/gentoo/.config          # kernel config: /usr/src/linux/.config
+# wget https://github.com/johan-jansson/gentoo/fstab            # file system table: /etc/fstab
+# wget https://github.com/johan-jansson/gentoo/grub             # grub main config: /etc/default/grub
 
 source /etc/profile
 export PS1="chroot $PS1"
