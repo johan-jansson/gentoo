7f478ee8d30ff979c8f4d01670fc550cc4652e99
Johan Jansson <33960195+johan-jansson@users.noreply.github.com>
2020-11-21 18:39:46 +0100

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -51,20 +51,3 @@ useradd -m -G users,wheel,audio,video,input johan
 passwd johan
 passwd -d root
 exit
-
-# POST-POST-INSTALL
-emerge -uDN @world                    # necessary here?
-sudo emerge sys-apps/dbus elogind
-sudo rc-update add dbus default
-sudo rc-update add elogind boot
-sudo rc-update add elogind default
-sudo rc-service elogind start
-sudo emerge xorg-server xorg-drivers xrandr setxkbmap
-sudo emerge dwm st dmenu
-
-mkdir ~/scripts/
-!!cp startdwm ~/scripts/
-chmod +x ~/scripts/startdwm
-!!cp xinitrc ~/.xinitrc
-startx
-
